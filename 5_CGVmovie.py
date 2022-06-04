7680c79deb20fd7b8d6fb049625e95084756fbb0
IVY75 <100816713+IVY75@users.noreply.github.com>
2022-06-04 16:11:13 +0900

    Update 5_CGVmovie.py

--- 5_CGVmovie.py
+++ 5_CGVmovie.py
@@ -71,6 +71,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome('chromedriver', options = chrome_options)
 
 import time
 import pandas as pd
